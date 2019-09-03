48d70f700b20ab207d0c065f71fbb6ae1529ccb0
sharmaarindm <sharma.arindm@gmail.com>
2019-09-02 21:16:29 -0400

    added front end html for the home page using bootstrap

--- jobs/views.py
+++ jobs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
 
 def home(request):
-    return render((request, 'home.html'))
+    # will be looking for it inside the templates directory/modelname
+    return render(request, 'jobs/home.html')
